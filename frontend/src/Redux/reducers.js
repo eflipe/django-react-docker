52ecf4f74853f0627fd392c8876ec5f8db12a388
eflipe <felipecabaleiro@gmail.com>
2020-07-10 10:57:59 -0300

    Fixed general

--- frontend/src/Redux/reducers.js
+++ frontend/src/Redux/reducers.js
@@ -19,6 +19,7 @@ const postReducer = (state = initialState, action) => {
 
         case Types.DELETE_POST: {
             return {...state, posts: state.posts.filter(post => post.id !== action.payload)}
+
         }
         default:
             return state;
--- frontend/src/Redux/types.js
+++ frontend/src/Redux/types.js
@@ -2,7 +2,8 @@ const Types = {
     POSTS_LOADING: "POSTS_LOADING",
     GET_POSTS:"GET_POSTS",
     DELETE_POST:"DELETE_POST",
-    CREATE_POST:"CREATE_POST"
+    CREATE_POST:"CREATE_POST",
+
 };
 
 export default Types
